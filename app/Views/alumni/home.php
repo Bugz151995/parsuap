f478f159522088bf80730e672803cb862882a0b9
Bugz151995 <ryan.abugao@gmail.com>
2021-10-19 10:37:44 +0800

    home

--- app/Views/alumni/home.php
+++ app/Views/alumni/home.php
@@ -5,7 +5,7 @@
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
+  </div>g
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="<?= site_url()?>dist/images/3966983.jpg" class="d-block w-100" alt="...">
